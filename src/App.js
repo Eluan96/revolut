094811aff40cea0e1dbd06ee06398b2e071c0681
Eluan96 <eluan.ea@gmail.com>
2023-09-11 20:14:20 +0100

    start

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Hero from './Componnet/hero/Hero';
+import Navbar from './Componnet/navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar />
+        <Hero />
+    </div>
+  );
+}
+
+export default App;
